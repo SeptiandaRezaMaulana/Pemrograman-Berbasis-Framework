165b43c421f74cf338312c61eee3f3d1215373cd
SeptiandaRezaMaulana <septiandareza07@gmail.com>
2021-03-08 14:30:29 +0700

    Initial Commit

---
+++ reactjs/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Septianda Reza Maulana <code> 1841720135 </code>
+        </p>
+        <a
+          className="App-link"
+          href="https://www.instagram.com/siusiu.rm7/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Follow Me On Instagram
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
