7b7944c68684318d1a6611e960b1260f3c68cd9c
SeptiandaRezaMaulana <septiandareza07@gmail.com>
2021-03-15 14:32:49 +0700

    Update Code
    This code changes for week 6

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Septianda Reza Maulana <code> 1841720135 </code>
-        </p>
-        <a
-          className="App-link"
-          href="https://www.instagram.com/siusiu.rm7/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Follow Me On Instagram
-        </a>
-      </header>
+      <Navbar />
+      <Header />
+      <h2>Hello World Septianda Reza</h2>
+      <Footer />
     </div>
   );
 }
-
 export default App;
